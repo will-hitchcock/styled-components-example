caf4ccaddb06f5bde329a5f8525099766a9e2877
William Hitchcock <willh@getambassador.com>
2017-08-24 09:32:32 -0400

    -Cleanup demo grid

--- app/containers/GridDemo/index.js
+++ app/containers/GridDemo/index.js
@@ -23,22 +23,167 @@ const Box = styled.div`
   color: #fff;
 `
 
-const GridDemo = () =>
-  <Container>
+const GridDemo = (props) =>
+  <Container className={props.className}>
     <Row>
-      <Col xs={12} sm={6} lg={3}>
-        <Box />
+      <Col xs={12} sm={12} lg={12}>
+        <Box>
+          xs=12 sm=12 lg=12
+        </Box>
       </Col>
-      <Col xs={12} sm={6} lg={3}>
-        <Box />
+    </Row>
+
+    <Row>
+      <Col xs={6} sm={6} lg={6}>
+        <Box>
+          xs=6 sm=6 lg=6
+        </Box>
       </Col>
-      <Col xs={12} sm={6} lg={3}>
-        <Box />
+      <Col xs={6} sm={6} lg={6}>
+        <Box>
+          xs=6 sm=6 lg=6
+        </Box>
       </Col>
-      <Col xs={12} sm={6} lg={3}>
-        <Box />
+    </Row>
+
+    <Row>
+      <Col xs={4} sm={4} lg={4}>
+        <Box>
+          xs=4 sm=4 lg=4
+        </Box>
+      </Col>
+      <Col xs={4} sm={4} lg={4}>
+        <Box>
+          xs=4 sm=4 lg=4
+        </Box>
+      </Col>
+      <Col xs={4} sm={4} lg={4}>
+        <Box>
+          xs=4 sm=4 lg=4
+        </Box>
+      </Col>
+    </Row>
+
+    <Row>
+      <Col xs={3} sm={3} lg={3}>
+        <Box>
+          xs=3 sm=3 lg=3
+        </Box>
+      </Col>
+      <Col xs={3} sm={3} lg={3}>
+        <Box>
+          xs=3 sm=3 lg=3
+        </Box>
+      </Col>
+      <Col xs={3} sm={3} lg={3}>
+        <Box>
+          xs=3 sm=3 lg=3
+        </Box>
+      </Col>
+      <Col xs={3} sm={3} lg={3}>
+        <Box>
+          xs=3 sm=3 lg=3
+        </Box>
+      </Col>
+    </Row>
+
+    <Row>
+      <Col xs={2} sm={2} lg={2}>
+        <Box>
+          xs=2 sm=2 lg=2
+        </Box>
+      </Col>
+      <Col xs={2} sm={2} lg={2}>
+        <Box>
+          xs=2 sm=2 lg=2
+        </Box>
+      </Col>
+      <Col xs={2} sm={2} lg={2}>
+        <Box>
+          xs=2 sm=2 lg=2
+        </Box>
+      </Col>
+      <Col xs={2} sm={2} lg={2}>
+        <Box>
+          xs=2 sm=2 lg=2
+        </Box>
+      </Col>
+      <Col xs={2} sm={2} lg={2}>
+        <Box>
+          xs=2 sm=2 lg=2
+        </Box>
+      </Col>
+      <Col xs={2} sm={2} lg={2}>
+        <Box>
+          xs=2 sm=2 lg=2
+        </Box>
+      </Col>
+    </Row>
+
+    <Row>
+      <Col xs={1} sm={1} lg={1}>
+        <Box>
+          xs=1 sm=1 lg=1
+        </Box>
+      </Col>
+      <Col xs={1} sm={1} lg={1}>
+        <Box>
+          xs=1 sm=1 lg=1
+        </Box>
+      </Col>
+      <Col xs={1} sm={1} lg={1}>
+        <Box>
+          xs=1 sm=1 lg=1
+        </Box>
+      </Col>
+      <Col xs={1} sm={1} lg={1}>
+        <Box>
+          xs=1 sm=1 lg=1
+        </Box>
+      </Col>
+      <Col xs={1} sm={1} lg={1}>
+        <Box>
+          xs=1 sm=1 lg=1
+        </Box>
+      </Col>
+      <Col xs={1} sm={1} lg={1}>
+        <Box>
+          xs=1 sm=1 lg=1
+        </Box>
+      </Col>
+      <Col xs={1} sm={1} lg={1}>
+        <Box>
+          xs=1 sm=1 lg=1
+        </Box>
+      </Col>
+      <Col xs={1} sm={1} lg={1}>
+        <Box>
+          xs=1 sm=1 lg=1
+        </Box>
+      </Col>
+      <Col xs={1} sm={1} lg={1}>
+        <Box>
+          xs=1 sm=1 lg=1
+        </Box>
+      </Col>
+      <Col xs={1} sm={1} lg={1}>
+        <Box>
+          xs=1 sm=1 lg=1
+        </Box>
+      </Col>
+      <Col xs={1} sm={1} lg={1}>
+        <Box>
+          xs=1 sm=1 lg=1
+        </Box>
+      </Col>
+      <Col xs={1} sm={1} lg={1}>
+        <Box>
+          xs=1 sm=1 lg=1
+        </Box>
       </Col>
     </Row>
   </Container>
 
-export default GridDemo
+export default styled(GridDemo)`
+  margin-top: 2rem;
+`
